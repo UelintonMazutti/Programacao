31621930daef297bde23104cfcd6d84511754f0b
Uelinton Mazutti <uelinton85@gmail.com>
2025-10-06 19:15:55 -0300

    first commit

---
+++ Iniciar Servidor.bat
@@ -0,0 +1,11 @@
+@echo off
+REM Vai para a pasta do curso
+cd /d D:\Programacao
+
+REM Abre o navegador apontando para o arquivo inicial
+start http://localhost:8080/cursoc.html
+
+REM Inicia o servidor HTTP na porta 8080
+python -m http.server 8080
+
+pause

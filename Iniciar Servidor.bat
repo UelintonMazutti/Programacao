67edc21990bc1bd5328381903870237f43bf5e85
Uelinton Mazutti <uelinton85@gmail.com>
2025-10-07 19:12:14 -0300

    pronto

--- Iniciar Servidor.bat
+++ Iniciar Servidor.bat
@@ -3,7 +3,7 @@ REM Vai para a pasta do curso
 cd /d D:\Programacao
 
 REM Abre o navegador apontando para o arquivo inicial
-start http://localhost:8080/curso.html
+start http://localhost:8080/index.html
 
 REM Inicia o servidor HTTP na porta 8080
 python -m http.server 8080
